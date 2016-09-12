3c593357b4c61853846c9590e20e57d83f91faa9
githubLins <1203624432@qq.com>
2016-09-12 18:52:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cathleen/tel/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cathleen.tel;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:18810104170"));
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+}
